edce8e87b9abc1c16c9f9b184ad064d85cf40f9f
OlegDev <olgmrko.dev@gmail.com>
2024-10-14 16:55:57 +0200

    Settings commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "all",
+};
+
+export default config;
